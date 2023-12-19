7076f5e0080a26a3d5ea3a1ea51411ca9d27d27a
Anderson Sinaluisa <afullink@gmail.com>
2023-12-19 12:33:00 -0500

    configuración de carpetas

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"src",
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@domain': '/src/domain',
+      '@components': '/src/components',
+      '@infrastructure': '/src/infrastructure',
+      '@interfaces': '/src/interfaces',
+    },
+  },
 })
