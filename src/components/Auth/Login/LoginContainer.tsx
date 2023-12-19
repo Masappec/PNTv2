78ecfc528fb7b293198037f6276cb8f587976cc0
Anderson Sinaluisa <afullink@gmail.com>
2023-12-19 12:04:41 -0500

    initial commit

---
+++ src/components/Auth/Login/LoginContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginContainer: React.FC = () => {
+
+    return (
+        <>
+            <h1>LoginContainer</h1> 
+        </>
+    )
+}
+export default LoginContainer
