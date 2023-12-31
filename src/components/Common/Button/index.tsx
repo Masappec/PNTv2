45e97e7bf8d2931fb0160320f969c16899776c47
Yessi-P <jessyrodri1990@gmail.com>
2023-12-31 10:58:43 -0500

    Revert "creacion de componente spinner"
    This reverts commit 32c5c9fb8ccad1583c082519a1de05f292b6ba4e.

--- src/components/Common/Button/index.tsx
+++ src/components/Common/Button/index.tsx
@@ -1,36 +1,11 @@
-import React,{useState} from "react";
-import Spinner from "../Spinner";
-
 
 const Button = ({ title, width }: { title: string, width: string }) => {
-  const [loading, setLoading] = useState(false);
-
- const handleButton = async (e) => {
-  e.preventDefault();
-  setLoading(true);
-
-  // Aquí iría el código para realizar la solicitud al servidor y verificar las credenciales
-
-  setTimeout(() => {
-    setLoading(false);
-  }, 2000);
-};
-
-
-  
   return (
     <>
 
-      <button className={' bg-sky-500 rounded-full py-2 md:ml-8 max-h-screen text-white hover:bg-orange-300 duration-500 ' + width} disabled={loading} onClick={handleButton}>
-      {loading ? (
-          <Spinner/>
-        ) : (
-         title   
-        )}
-             
+      <button className={' bg-sky-500 rounded-full py-2 md:ml-8 max-h-screen text-white hover:bg-orange-300 duration-500 ' + width}>
+        {title}
       </button>
-      
-
 
     </>
 
