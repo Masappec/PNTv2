d5780a2d85c10bf60998a86b3a20aab61728336a
Anderson Sinaluisa <afullink@gmail.com>
2024-01-01 18:43:51 -0500

    lading v1

--- src/components/Landing/LandingPresenter.tsx
+++ src/components/Landing/LandingPresenter.tsx
@@ -1,10 +1,27 @@
+import Footer from "../Common/Footer";
+import FooterInfo from "../Common/FooterInfo";
 import Header from "../Common/Header"
+import Hero from "../Common/Hero";
+import Product from "../Common/Products";
+import hero_image from '../../assets/hero_image.svg';
 
 
 
 const LandingPresenter = () => {
     return (
-        <Header />
+        <>
+            <Header />
+            <Hero title="Busca en nuestro catálogo de Datos Abiertos y Solicita los que necesites" 
+            subtitle="Encontrarás datos de distintas fuentes" 
+            image={hero_image} imageAlt="Hero Image"
+            ctaLinkPrimary="/" ctaTextPrimary="Explorar"
+            ctaLinkSecondary="/login"
+            ctaTextSecondary="Solictar"
+            key={1} />
+            
+            <Product/>
+            <FooterInfo />
+        </>
     )
 
 }
