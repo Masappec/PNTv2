d64793d99dc04ec9140cdad30bbc2b246602abe5
Anderson Sinaluisa <afullink@gmail.com>
2024-01-01 19:11:23 -0500

    Update index.tsx

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -8,7 +8,7 @@ const Header = ()=>{
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <a href="https://flowbite.com" className="flex items-center">
                     <img src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">PNT</span>
                 </a>
                 <div className="flex items-center lg:order-2">
                     <Link to="/login" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Iniciar sesión</Link>
