9122250832f5592821f4404120a72ea4151f5158
Yessi-P <jessyrodri1990@gmail.com>
2024-02-04 12:47:39 -0500

    reestructura pagina de inicio

---
+++ src/components/Common/Card/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Card } from "flowbite-react";
+import { FaArrowRight } from "react-icons/fa";
+
+interface Props{
+    title:string; 
+    content:string;
+    backgroundColor:string;
+    text:string;
+
+}
+
+const CardConsulta = (props: Props )     => {
+    return(
+     
+
+
+           
+       <div className="  flex flex-col  w-[270px] h-[350px] bg-[#F7941D0D]  mt-20 text-left">
+          <h1 className=" flex text-lg font-bold tracking-tight text-gray-900 dark:text-white bg-white p-3">
+            {props.title}
+             </h1> 
+        <Card href="#" className={" rounded-none shadow-none border-transparent border-l-[1px] border-t-[1px] border-r-[1px] max-w-sm h-full flex " + props.backgroundColor}>
+          
+            <p className=" font-semibold text-slate-950 dark:text-gray-400 h-full mt-4 text-left">
+           {props.content}
+            </p>
+            <div className="flex container col-2 mr-8 space-x-4 ">
+           
+           <h1 className="font-semibold"> {props.text}</h1> 
+           <div className="flex items-end justify-between h-full  ">
+            <FaArrowRight size={30} className="text-gray-900 dark:text-white" />
+
+            </div></div>
+
+         
+        </Card> 
+        </div>
+    )
+
+
+}
+
+export default CardConsulta
--- src/components/Landing/Home/LandingPresenter.tsx
+++ src/components/Landing/Home/LandingPresenter.tsx
@@ -1,37 +1,60 @@
-
 import Hero from "../../Common/Hero";
 import CardQuestion from "../../Common/CardQuestion";
 import { FormattedMessage } from "react-intl";
-
+import CardConsulta from "../../Common/Card";
 
 
 const LandingPresenter = () => {
-    return (
-        <div className="flex flex-col w-full bg-white pr-10">
-            <div>
+  return (
+    <div className="flex flex-col w-full  bg-white pr-10">
+      <div></div>
+      <div className="border-l-2 border-gray-900 ml-0 md:ml-10">
+        <Hero />
 
-            </div>
-            <div className="border-l-2 border-gray-900 ml-0 md:ml-10">
-                <Hero />
-
-
-                <div className="flex flex-row  w-full border-b">
-                    {
-                        Array.from({ length: 3 }).map((_, i) => (
-                            <CardQuestion key={i} />
-                        ))
-                    }
-                </div>
-                <div className="flex flex-row w-full mt-28 ">
-                    <p className="text-3xl ml-5  text-gray-800 dark:text-white  w-52" tabIndex={7}>
-                        <FormattedMessage id="search_by_tematic"/>
-                    </p>
-                    <hr className="w-full ml-5  border-gray-900 mt-10" />
-                </div>
-            </div>
+        <div className="flex flex-row  w-full border-b">
+          {Array.from({ length: 3 }).map((_, i) => (
+            <CardQuestion key={i} />
+          ))}
         </div>
-    )
+        <div className="flex  w-full mt-28 ">
+          <p
+            className="text-3xl ml-5  text-gray-800 dark:text-white  w-52 "
+            tabIndex={7}
+          >
+            <FormattedMessage id="search_by_tematic" />
+          </p>
+          <hr className="w-full ml-5  border-gray-900 mt-10" />
+        </div>
 
-}
+        <div className="flex  w-full justify-center space-x-10 mb-48 ">
+          <CardConsulta
+            title="Personal y salarios"
+            backgroundColor="  bg-yellow-400"
+            content="Encontrarás el directorio de los funcionarios públicos, las leyes, reglamentos, declaraciones juradas de los funcionarios y demás disposiciones legales que rigen y emiten las entidades."
+            text="CONSULTAR"
+          ></CardConsulta>
+          <CardConsulta
+            title="Presupuestos"
+            backgroundColor=" bg-slate-200"
+            content="Encontrarás los instrumentos de gestión, planes y políticas, que regulan las actividades de la entidad."
+            text=""
+          ></CardConsulta>
+          <CardConsulta
+            title="Contratos"
+            backgroundColor="bg-slate-200"
+            content=" Encontrarás información sobre presupuesto participativo, consejo de coordinación regional, audiencias públicas, entre otros."
+            text=""
+          ></CardConsulta>
+          <CardConsulta
+            title="Formularios y Solicitudes"
+            backgroundColor="bg-slate-200"
+            content="Encontrarás información de los proyectos de inversión y obras públicas de la entidad y su ejecución."
+            text=""
+          ></CardConsulta>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default LandingPresenter;
