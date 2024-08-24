809fe5391c404b138fd0e569fe81916ac142fec5
Yessi-P <jessyrodri1990@gmail.com>
2024-08-24 00:08:36 -0500

    cambios

--- src/components/Transparency/Solicity/Manual/SolicityManualContainer.tsx
+++ src/components/Transparency/Solicity/Manual/SolicityManualContainer.tsx
@@ -246,7 +246,7 @@ const SolicityManualContainer = (props: Props) => {
     ]
 
     const format_receipt: ColourOption[] = [
-        { value: 'formulario web', label: 'formulario web', color: '#00B8D9' },
+        // { value: 'formulario web', label: 'formulario web', color: '#00B8D9' },
         { value: 'Presencial', label: 'Presencial', color: '#00B8D9' },
         { value: 'Correo Electrónico', label: 'Correo Electrónico', color: '#00B8D9' },
         { value: 'Otro', label: 'Otro', color: '#00B8D9' },
