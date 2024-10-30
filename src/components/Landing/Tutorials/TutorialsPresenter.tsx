78172324b405357b6ff8a76c5f1b2635cb4edb68
Anderson Sinaluisa <andersonsinaluisa@gmail.com>
2024-10-30 09:48:39 -0500

    Update TutorialsPresenter.tsx

--- src/components/Landing/Tutorials/TutorialsPresenter.tsx
+++ src/components/Landing/Tutorials/TutorialsPresenter.tsx
@@ -37,30 +37,35 @@ const TutorialsPresenter = (props:Props) => {
         return (
             <main>
                  <MapIsotipo />
-            <section id='videos' className='section-container my-16'>
-              <h2 className='mb-4 text-balance text-2xl font-normal leading-tight md:text-[40px]'>
-                ¿Qué información voy a encontrar?
-              </h2>
-        
-              <p className='mb-8 text-sm text-gray-600 md:text-base'>
-                En este espacio encontrarás videos tutoriales que te ayudarán a navegar por el Portal
-                Nacional de Transparencia.
-              </p>
-        
-              <div className='space-y-8'>
-            
-              {
-                props.tutorial.map((data) => {
-                  return (
-                    <>
-                   
-                      <Iframe title={data.title} link={formatUrlYoutube(data.url)} />
-                    </>
-                  )
-                })
-              }
-            </div>
-            </section>
+                  <section id='videos' className='section-container my-16'>
+                    <h2 className='mb-4 text-balance text-2xl font-normal leading-tight md:text-[40px]'>
+                      ¿Qué información voy a encontrar?
+                    </h2>
+              
+                    <p className='mb-8 text-sm text-gray-600 md:text-base'>
+                      En este espacio encontrarás videos tutoriales que te ayudarán a navegar por el Portal
+                      Nacional de Transparencia.
+                    </p>
+              
+                    <div className='space-y-8'>
+                  
+                    {
+                      props.tutorial.map((data) => {
+                        return (
+                          <>
+                              <h2 className="font-bold text-2xl flex justify-start">
+                                {data.title}
+                              </h2>
+                            <p className="flex justify-start">
+                                {data.description}
+                            </p>
+                              <Iframe title={data.title} link={formatUrlYoutube(data.url)} />
+                          </>
+                        )
+                      })
+                    }
+                  </div>
+                  </section>
             </main>
            
         );
