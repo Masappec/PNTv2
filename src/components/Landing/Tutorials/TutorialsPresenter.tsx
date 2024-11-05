34a02947f096ab38817aa489adc35feb7da385e7
ChaeMendoza <israelmendoza656@outlook.com>
2024-11-05 11:39:09 -0500

    Add: Title and descriptions for params

--- src/components/Landing/Tutorials/TutorialsPresenter.tsx
+++ src/components/Landing/Tutorials/TutorialsPresenter.tsx
@@ -1,71 +1,66 @@
-import { FrequencyAsked, Tutorial } from "../../../domain/entities/PedagodyAreaEntity";
+import {
+  FrequencyAsked,
+  Tutorial,
+} from "../../../domain/entities/PedagodyAreaEntity";
 import Iframe from "../../Common/Iframe";
 import { MapIsotipo } from "../../Common/MapIsotipo";
 
-
-
 interface Props {
-    tutorial: Tutorial[];
-    faq: FrequencyAsked[];
-    loading: boolean;
-    error: string;
-    
+  tutorial: Tutorial[];
+  faq: FrequencyAsked[];
+  loading: boolean;
+  error: string;
 }
-const TutorialsPresenter = (props:Props) => {
+const TutorialsPresenter = (props: Props) => {
+  const formatUrlYoutube = (url: string) => {
+    if (url.includes("embed")) return url;
 
-    const formatUrlYoutube = (url: string) => {
-      
-      if (url.includes('embed')) return url;
+    if (
+      url.includes("youtube") ||
+      (url.includes("youtu.be") && !url.includes("embed"))
+    ) {
+      const urlParts = url.split("/");
 
-      if (url.includes('youtube') || url.includes('youtu.be') && !url.includes('embed')) {
-
-          const urlParts = url.split('/');
-
-
-          const videoId = urlParts[urlParts.length - 1];
-          return `https://www.youtube.com/embed/${videoId}`;
-
-      }
-      console.log(url)
-
-      return url;
-
-      
-
+      const videoId = urlParts[urlParts.length - 1];
+      return `https://www.youtube.com/embed/${videoId}`;
     }
+    console.log(url);
 
-        return (
-            <main>
-                 <MapIsotipo />
-            <section id='videos' className='section-container my-16'>
-              <h2 className='mb-4 text-balance text-2xl font-normal leading-tight md:text-[40px]'>
-                ¿Qué información voy a encontrar?
-              </h2>
-        
-              <p className='mb-8 text-sm text-gray-600 md:text-base'>
-                En este espacio encontrarás videos tutoriales que te ayudarán a navegar por el Portal
-                Nacional de Transparencia.
-              </p>
-        
-              <div className='space-y-8'>
-            
-              {
-                props.tutorial.map((data) => {
-                  return (
-                    <>
-                   
-                      <Iframe title={data.title} link={formatUrlYoutube(data.url)} />
-                    </>
-                  )
-                })
-              }
-            </div>
-            </section>
-            </main>
-           
-        );
-    }
-export default TutorialsPresenter; 
+    return url;
+  };
 
+  return (
+    <main>
+      <MapIsotipo />
+      <section id="videos" className="section-container my-16">
+        <h2 className="mb-4 text-balance text-2xl font-normal leading-tight md:text-[40px]">
+          ¿Qué información voy a encontrar?
+        </h2>
 
+        <p className="mb-8 text-sm text-gray-600 md:text-base">
+          En este espacio encontrarás videos tutoriales que te ayudarán a
+          navegar por el Portal Nacional de Transparencia.
+        </p>
 
+        <div className="space-y-8">
+          {props.tutorial.map((data) => {
+            return (
+              <>
+                <hr />
+                <h2 className="text-center text-2xl font-semibold text-gray">
+                  {data.title}
+                </h2>
+                <Iframe title={data.title} link={formatUrlYoutube(data.url)} />
+                <p className="text-center text-gray-700 text-lg">
+                  {data.description}
+                </p>
+                <hr />
+              </>
+            );
+          })}
+        </div>
+      </section>
+    </main>
+  );
+};
+export default TutorialsPresenter;
