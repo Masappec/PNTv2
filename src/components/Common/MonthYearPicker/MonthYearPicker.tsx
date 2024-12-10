ad756e442c0038ac06721bdac1e2a8aa39a46962
IsraMendoza <israel656mendoza@gmail.com>
2024-12-10 00:24:46 -0500

    FIX: Cambios en MonthYearPicker para build

--- src/components/Common/MonthYearPicker/MonthYearPicker.tsx
+++ src/components/Common/MonthYearPicker/MonthYearPicker.tsx
@@ -2,23 +2,34 @@ import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-function MonthYearPicker({ onChangeDate }) {
-  const [selectedDate, setSelectedDate] = useState(null);
+interface MonthYearPickerProps {
+  onChangeDate: (date: string) => void; // El callback ahora espera un string
+}
+
+function MonthYearPicker({ onChangeDate }: MonthYearPickerProps) {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+    if (date) {
+      // Convertir el objeto Date a formato YYYY-MM
+      const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}`; // Dos digitos
+      onChangeDate(formattedDate);
+    }
+  };
 
   return (
     <DatePicker
       selected={selectedDate}
-      onChange={(date) => {
-        setSelectedDate(date);
-        onChangeDate(date); // Notifica al padre
-      }}
+      onChange={handleDateChange}
       dateFormat="MM/yyyy"
       showMonthYearPicker
-      placeholderText="Selecciona un periodo 🗓️"
+      placeholderText="Selecciona un periodo"
       className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-8 text-sm text-gray-900 outline-primary focus:border-cyan-500 focus:ring-cyan-500 disabled:cursor-not-allowed disabled:opacity-50"
     />
   );
 }
-
 
 export default MonthYearPicker;
