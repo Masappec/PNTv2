35a9c58a4291f8711dfc99afef2cbe61bfc16134
Anderson Sinaluisa <afullink@gmail.com>
2024-12-28 12:43:05 -0500

    Update ColumnMapper.ts

--- src/domain/mappers/ColumnMapper.ts
+++ src/domain/mappers/ColumnMapper.ts
@@ -1,31 +1,32 @@
 import { ColumnTemplateDto } from "../../infrastructure/Api/TemplateFile/inteface";
+import SessionService from "../../infrastructure/Services/SessionService";
 import ColumnTemplate from "../entities/ColumTemplate";
 
 
+
 export class ColumnMapper {
-    static toDomain(column: ColumnTemplateDto): ColumnTemplate {
-        return {
-            code: column.code,
-            format: column.format,
-            id: column.id,
-            name: column.name,
-            regex: column.regex,
-            type: column.type,
-            value:column.value
-        }
-    }
+  static establishmentName: string = SessionService.getEstablishmentData()?.name||"";
+  static toDomain(column: ColumnTemplateDto): ColumnTemplate {
+    return {
+      code: column.code,
+      format: column.format,
+      id: column.id,
+      name: column.name,
+      regex: column.regex,
+      type: column.type,
+      value: column.value.replace("{INSTITUCION}", ColumnMapper.establishmentName),
+    };
+  }
 
-
-
-    static toApi(column: ColumnTemplate): ColumnTemplateDto {
-        return {
-            code: column.code,
-            format: column.format || '',
-            id: column.id,
-            name: column.name,
-            regex: column.regex || '',
-            type: column.type,
-            value:column.value
-        }
-    }
+  static toApi(column: ColumnTemplate): ColumnTemplateDto {
+    return {
+      code: column.code,
+      format: column.format || "",
+      id: column.id,
+      name: column.name,
+      regex: column.regex || "",
+      type: column.type,
+      value: column.value,
+    };
+  }
 }
