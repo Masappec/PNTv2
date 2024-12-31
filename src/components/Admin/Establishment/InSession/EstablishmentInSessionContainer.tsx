25dd981b2ee10c0420ec2f04f7dec188e5461913
ChaeMendoza <israel656mendoza@outlook.com>
2024-12-31 09:25:39 -0500

    Fix: traer las obligaciones seleccionadas anteriormente

--- src/components/Admin/Establishment/InSession/EstablishmentInSessionContainer.tsx
+++ src/components/Admin/Establishment/InSession/EstablishmentInSessionContainer.tsx
@@ -91,7 +91,7 @@ const EstablishmentInSessionContainer = ({
         usecase.getByUserSession().then((res) => {
             const es = res
             numeralUsecase.getNumeralByEstablishment(es.id || 0).then((res) => {
-                const res_ = res.filter((item) => !item.isDefault && !item.isSelected).map((item) => item.id)
+                const res_ = res.filter((item) => !item.isDefault).map((item) => item.id)
                 setData({ ...es, extra_numerals: res_.join(',') })
             }).catch((err) => {
                 console.log(err)
