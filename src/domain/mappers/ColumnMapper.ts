84c4a61e7a512b44c1a5cb4e79ba18dba93e549f
Anderson Sinaluisa <afullink@gmail.com>
2025-01-06 22:29:37 -0500

    Update ColumnMapper.ts

--- src/domain/mappers/ColumnMapper.ts
+++ src/domain/mappers/ColumnMapper.ts
@@ -1,19 +1,22 @@
 import { ColumnTemplateDto } from "../../infrastructure/Api/TemplateFile/inteface";
+import SessionService from "../../infrastructure/Services/SessionService";
 import ColumnTemplate from "../entities/ColumTemplate";
 
 
 export class ColumnMapper {
-    static toDomain(column: ColumnTemplateDto): ColumnTemplate {
-        return {
-            code: column.code,
-            format: column.format,
-            id: column.id,
-            name: column.name,
-            regex: column.regex,
-            type: column.type,
-            value:column.value
-        }
-    }
+
+  static toDomain(column: ColumnTemplateDto): ColumnTemplate {
+    const establishmentName = SessionService.getEstablishmentData()?.name||"";
+    return {
+      code: column.code,
+      format: column.format,
+      id: column.id,
+      name: column.name,
+      regex: column.regex,
+      type: column.type,
+      value: column.value.replace("{INSTITUCION}", establishmentName),
+    };
+  }
 
 
 
