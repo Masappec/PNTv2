4f65de062a8528464647209cca4045b81cd9e2a5
andersonSinaluisa <55563658+andersonSinaluisa@users.noreply.github.com>
2025-01-16 22:56:54 -0500

    Update SolicityResponseContainer.tsx

--- src/components/Transparency/Solicity/Response/SolicityResponseContainer.tsx
+++ src/components/Transparency/Solicity/Response/SolicityResponseContainer.tsx
@@ -703,7 +703,6 @@ const SolicityResponseContainer = (props: Props) => {
                     isAvaliableForChangeStatus={IsChangeStatus}
                     textForChangeStatus={textChangeStatus()}
                     textForMotiveDescription={textDescription}
-                    isAvaliableToProrroga={props.usecase.avaliableToProrroga(userSession, solicityToResponse)}
                 /> : null
             }
         </SolicityDetailContainer >
