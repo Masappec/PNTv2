67de7b08c4433de7570d8fb0b6f3d22da01ff98d
Anderson Sinaluisa <andersonsinaluisa@gmail.com>
2025-01-20 10:44:53 -0500

    Update AnualReportUseCase.ts

--- src/domain/useCases/AnualReportUseCase/AnualReportUseCase.ts
+++ src/domain/useCases/AnualReportUseCase/AnualReportUseCase.ts
@@ -14,22 +14,22 @@ export class AnualReportUseCase {
     data.year = new DatePnt().getYearToUpload();
 
     if (data.have_public_records) {
-      if (data.norme_archive_utility.trim().length === 0) {
+      if (data.norme_archive_utility.trim().length == 0) {
         throw new Error(
           "Norma archivística utilizada: no puede estar vacía"
         );
       }
-      if (data.comment_aclaration.trim().length === 0) {
+      if (data.comment_aclaration.trim().length == 0) {
         throw new Error("Comentario aclaratorio no puede estar vacío");
       }
     }
 
     if (data.have_quality_problems) {
-      if (data.total_quality_problems === 0) {
+      if (data.total_quality_problems < 0) {
         throw new Error("Cantidad de gestiones oficiosas no puede ser 0");
       }
 
-      if (data.description_quality_problems.trim().length === 0) {
+      if (data.description_quality_problems.trim().length == 0) {
         throw new Error(
           "Descripción específica de la corrección de información no puede estar vacía"
         );
@@ -37,50 +37,50 @@ export class AnualReportUseCase {
     }
 
     if (data.have_sanctions) {
-      if (data.total_organic_law_public_service === 0) {
+      if (data.total_organic_law_public_service < 0) {
         throw new Error(
           "Cantidad de sanciones por la ley orgánica del servicio público no puede ser 0"
         );
       }
 
-      if (data.description_organic_law_public_service.trim().length === 0) {
+      if (data.description_organic_law_public_service.trim().length == 0) {
         throw new Error(
           "Descripción específica de sanciones por la ley orgánica del servicio público no puede estar vacía"
         );
       }
 
-      if (data.total_organic_law_contraloria === 0) {
+      if (data.total_organic_law_contraloria  < 0) {
         throw new Error(
           "Cantidad de sanciones por la ley orgánica de la contraloría no puede ser 0"
         );
       }
 
-      if (data.description_organic_law_contraloria.trim().length === 0) {
+      if (data.description_organic_law_contraloria.trim().length == 0) {
         throw new Error(
           "Descripción específica de sanciones por la ley orgánica de la contraloría no puede estar vacía"
         );
       }
 
-      if (data.total_organic_law_national_system === 0) {
+      if (data.total_organic_law_national_system < 0) {
         throw new Error(
           "Cantidad de sanciones por la ley orgánica del sistema nacional de control no puede ser 0"
         );
       }
 
-      if (data.description_organic_law_national_system.trim().length === 0) {
+      if (data.description_organic_law_national_system.trim().length == 0) {
         throw new Error(
           "Descripción específica de sanciones por la ley orgánica del sistema nacional de control no puede estar vacía"
         );
       }
 
-      if (data.total_organic_law_citizen_participation === 0) {
+      if (data.total_organic_law_citizen_participation < 0) {
         throw new Error(
           "Cantidad de sanciones por la ley orgánica de participación ciudadana no puede ser 0"
         );
       }
 
       if (
-        data.description_organic_law_citizen_participation.trim().length === 0
+        data.description_organic_law_citizen_participation.trim().length == 0
       ) {
         throw new Error(
           "Descripción específica de sanciones por la ley orgánica de participación ciudadana no puede estar vacía"
@@ -88,42 +88,42 @@ export class AnualReportUseCase {
       }
     }
     if (data.have_activities) {
-      if (data.total_activities === 0) {
+      if (data.total_activities < 0) {
         throw new Error("Cantidad de actividades no puede ser 0");
       }
 
-      if (data.description_activities.trim().length === 0) {
+      if (data.description_activities.trim().length == 0) {
         throw new Error("Descripción de actividades no puede estar vacía");
       }
     }
 
     if (data.did_you_entity_receive) {
-      if (data.total_saip_in_portal === 0) {
+      if (data.total_saip_in_portal < 0) {
         throw new Error(
           "Cantidad de solicitudes de información pública recibidas en el portal no puede ser 0"
         );
       }
 
-      if (data.total_saip_no_portal === 0) {
+      if (data.total_saip_no_portal < 0) {
         throw new Error(
           "Cantidad de solicitudes de información pública recibidas fuera del portal no puede ser 0"
         );
       }
 
-      if (data.description_rason_no_portal.trim().length === 0) {
+      if (data.description_rason_no_portal.trim().length == 0) {
         throw new Error(
           "Descripción de razón de solicitudes no recibidas en el portal no puede estar vacía"
         );
       }
 
       if (data.have_responded_solicities_no_portal) {
-        if (data.total_no_registered === 0) {
+        if (data.total_no_registered < 0) {
           throw new Error(
             "Cantidad de solicitudes no registradas no puede ser 0"
           );
         }
 
-        if (data.comment_aclaration_no_registered.trim().length === 0) {
+        if (data.comment_aclaration_no_registered.trim().length == 0) {
           throw new Error(
             "Comentario aclaratorio de solicitudes no registradas no puede estar vacío"
           );
@@ -132,19 +132,19 @@ export class AnualReportUseCase {
     }
 
     if (data.reserve_information) {
-      if (data.number_of_reserves === 0) {
+      if (data.number_of_reserves < 0) {
         throw new Error("Cantidad de información reservada no puede ser 0");
       }
 
-      if (data.number_of_confidential === 0) {
+      if (data.number_of_confidential < 0) {
         throw new Error("Cantidad de información confidencial no puede ser 0");
       }
 
-      if (data.number_of_secret === 0) {
+      if (data.number_of_secret < 0) {
         throw new Error("Cantidad de información secreta no puede ser 0");
       }
 
-      if (data.number_of_secretism === 0) {
+      if (data.number_of_secretism < 0) {
         throw new Error("Cantidad de información ultra secreta no puede ser 0");
       }
     }
