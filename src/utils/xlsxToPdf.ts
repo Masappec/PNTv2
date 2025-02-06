e77894fd12ef69f6627378e82f85e77331c50462
andersonSinaluisa <55563658+andersonSinaluisa@users.noreply.github.com>
2025-02-05 23:18:59 -0500

    descarga pdf, csv informe anual

--- src/utils/xlsxToPdf.ts
+++ src/utils/xlsxToPdf.ts
@@ -1,8 +1,10 @@
 import * as XLSX from "xlsx";
 import { jsPDF } from "jspdf";
 import autoTable from "jspdf-autotable";
+import axios from "axios";
+import ExcelJS from 'exceljs';
 
-export const fetchAndConvertToPdf = async (url: string, establishment_name: string, year: string) => {
+export const fetchAndConvertToPdfAnualReportEstablishment = async (url: string, establishment_name: string, year: string) => {
     if (!url) {
         console.error("URL no válida");
         return;
@@ -108,5 +110,154 @@ export const fetchAndConvertToPdf = async (url: string, establishment_name: stri
     } catch (error) {
         console.error("Error al descargar o procesar el archivo:", error);
         alert("No se pudo descargar el archivo. Verifica la URL.");
+    }
+};
+
+export const fetchAndConvertToPdfAnualReport = async (url: string, filename: string, year: number) => {
+    if (!url) {
+        console.error("URL no válida");
+        return;
+    }
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(`Error al descargar el archivo: ${response.statusText}`);
+
+        const excelBlob = await response.blob();
+        const arrayBuffer = await excelBlob.arrayBuffer();
+
+        // Cargar el archivo Excel con ExcelJS
+        const workbook = new ExcelJS.Workbook();
+        await workbook.xlsx.load(arrayBuffer);
+
+        const doc = new jsPDF("landscape");
+        doc.text(`Informe Anual ${year}`, 20, 10);
+        workbook.worksheets.forEach((sheet, ) => {
+            const sheetName = sheet.name;
+            // Leer las filas y columnas restantes
+            const jsonData: any[][] = [];
+            sheet.eachRow({ includeEmpty: false }, (row, ) => {
+                const rowData: any[] = [];
+                row.eachCell({ includeEmpty: true }, (cell, ) => {
+                    rowData.push(cell.value || "");
+                });
+                jsonData.push(rowData);
+            });
+
+            // Agregar la hoja al PDF si tiene datos
+            if (jsonData && jsonData.length > 0) {
+                doc.text(sheetName, 25, 20);
+
+                autoTable(doc, {
+                    head: [jsonData[0]], // Primer fila como encabezado
+                    body: jsonData.slice(1), // Las siguientes filas como datos
+                    startY:30,
+                    margin: { bottom: 15 },
+                });
+            } else {
+                console.warn(`La hoja "${sheetName}" no tiene datos válidos.`);
+            }
+        });
+
+        // Guardar el archivo PDF
+        doc.save(`${filename}.pdf`);
+    } catch (error) {
+        console.error("Error al descargar o procesar el archivo:", error);
+        alert("No se pudo descargar el archivo. Verifica la URL.");
+    }
+};
+
+
+export const fetchAndConvertToCsvAnualReport = async (url: string, filename: string, year: number) => {
+    if (!url) {
+        console.error("URL no válida");
+        return;
+    }
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) throw new Error(`Error al descargar el archivo: ${response.statusText}`);
+
+        const _blob = await response.blob();
+        const arrayBuffer = await _blob.arrayBuffer();
+
+        // Cargar el archivo Excel con ExcelJS
+        const workbook = new ExcelJS.Workbook();
+        await workbook.xlsx.load(arrayBuffer);
+
+        // Crear un array para almacenar todas las filas CSV
+        let csvContent = '';
+
+        // Iterar sobre las hojas y agregar todas las filas en el mismo CSV
+        workbook.worksheets.forEach((sheet, ) => {
+            const sheetName = sheet.name;
+            console.log(`Procesando hoja para CSV: ${sheetName}`);
+
+            // Agregar el nombre de la hoja como título
+            csvContent += `\n\n--- ${sheetName} ---\n`; // Agregar un salto de línea y el nombre de la hoja
+
+            // Recorrer las filas de la hoja
+            sheet.eachRow({ includeEmpty: true }, (row, ) => {
+                const rowData: string[] = [];
+                
+                // Recorrer las celdas de cada fila
+                row.eachCell({ includeEmpty: true }, (cell, ) => {
+                    rowData.push(`"${cell.value || ''}"`); // Aseguramos que el valor esté entre comillas
+                });
+
+                // Unir las celdas por comas y agregar la fila al contenido CSV
+                csvContent += rowData.join(';') + '\n';
+            });
+        });
+
+        csvContent = '\uFEFF' + csvContent; // Agregar BOM para compatibilidad con Excel
+        // Crear un enlace para descargar el archivo CSV
+        const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = `${filename}_${year}_ReporteAnual.csv`; // Nombre del archivo CSV
+
+        // Simular clic para descargar el archivo
+        link.click();
+    } catch (error) {
+        console.error("Error al descargar o procesar el archivo:", error);
+        alert("No se pudo descargar el archivo. Verifica la URL.");
+    }
+};
+
+                                    
+export const downloadExcelAndCombineToCSV = async (excelUrl: string, name: string) => {
+    try {
+        // Descargar el archivo Excel
+        const response = await axios.get(excelUrl, { responseType: 'arraybuffer' });
+        const data = new Uint8Array(response.data);
+
+        // Leer el archivo Excel
+        const workbook = XLSX.read(data, { type: 'array' });
+
+        // Combinar el contenido de todas las hojas en un solo CSV
+        let combinedCSV = '';
+
+        workbook.SheetNames.forEach(sheetName => {
+            const worksheet = workbook.Sheets[sheetName];
+            const csv = XLSX.utils.sheet_to_csv(worksheet, { FS: ';' }); // Usar punto y coma como separador
+
+            // Añadir nombre de la hoja como encabezado
+            combinedCSV += `\n \n --- ${sheetName} ---\n${csv}\n`;
+        });
+
+        combinedCSV = '\uFEFF' + combinedCSV; // Agregar BOM para compatibilidad con Excel
+        // Crear un blob y generar un enlace de descarga
+        const blob = new Blob([combinedCSV], { type: 'text/csv;charset=utf-8;' });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', name+'.csv');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+    } catch (error) {
+        console.error('Error durante la descarga o conversión:', error);
     }
 };
